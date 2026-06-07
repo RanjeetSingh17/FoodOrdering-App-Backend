8949f84a82c2b4cbc48f372f0534b8ccce94b419
RanjeetSingh17 <ranjeetsingh71717@gmail.com>
2026-06-07 18:13:51 +0530

    Middleware Changed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,41 @@
+// const admin = require('firebase-admin');
+// admin.initializeApp({
+//   credential: admin.credential.cert(require('../config/serviceKey.json')),
+// });
+// const verifyToken = async (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//     return res.status(403).json({ error: 'Unauthorized' });
+//   }
+
+//   const token = authHeader.split('Bearer ')[1];
+
+//   try {
+//     const decodedToken = await admin.auth().verifyIdToken(token);
+//     const uid = decodedToken.uid;
+//     req.user = { uid };
+//     next();
+//   } catch (error) {
+//     console.error('Error verifying token:', error.message);
+//     return res.status(403).json({ error: 'Invalid or expired token' });
+//   }
+// };
+ 
+// module.exports= verifyToken
+
 const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
 admin.initializeApp({
-  credential: admin.credential.cert(require('../config/serviceKey.json')),
+  credential: admin.credential.cert(serviceAccount),
 });
+
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(403).json({ error: 'Unauthorized' });
   }
@@ -12,13 +44,12 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
-    const uid = decodedToken.uid;
-    req.user = { uid };
+    req.user = { uid: decodedToken.uid };
     next();
   } catch (error) {
     console.error('Error verifying token:', error.message);
     return res.status(403).json({ error: 'Invalid or expired token' });
   }
 };
- 
-module.exports= verifyToken
+
+module.exports = verifyToken;
